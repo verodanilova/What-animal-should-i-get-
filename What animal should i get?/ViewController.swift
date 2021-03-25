1ffdbedc7296cff8f9f832ac26424688a40d318e
Tatyana <kushnirchuktanya@gmail.com>
2021-03-25 14:17:11 +0300

    Initial Commit

---
+++ What animal should i get?/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What animal should i get?
+//
+//  Created by Tanya on 25.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
